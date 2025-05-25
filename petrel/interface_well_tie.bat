1be404931a6d682a91c66f8f2886cfe9b9724c90
Provavelmente-o-Caio <caiofecardoso@hotmail.com>
2025-05-25 19:23:21 -0300

    Ajuste na interface petrel-python

--- petrel/interface_well_tie.bat
+++ petrel/interface_well_tie.bat
@@ -10,19 +10,23 @@ set CONDA_ENV_ZIP=dlseis_well_tie.zip
 set CONDA_ENV_NAME=wtie
 
 set PATH_CONDA=D:\anaconda3
+set EXTRACT_PATH=%AppData%
 
-if not exist "%AppData%\teste.py" (
-    echo Descompactando script python
-    "%~dp0\7z.exe" x "%~dp0\%CONDA_ENV_ZIP" -o"%AppData%"
+
+if not exist "%AppData%\dlseis_well_tie\petrel\interface2.py" (
+	echo Descompactando script python
+	"%~dp07z.exe" x "%~dp0%CONDA_ENV_ZIP%" -o%EXTRACT_PATH%
+	pause
 )
 
 call %PATH_CONDA%\Scripts\activate.bat
 
 if not exist "%PATH_CONDA%\envs\%CONDA_ENV_NAME%" (
     echo no env
-    call conda env create -f "%CAMINHO%\dlseis_well_tie\enviroment_windows.yml"
+    call conda env create -f "%AppData%\dlseis_well_tie\enviroment_windows.yml"
 )
 
+echo activating env
 call conda activate "%CONDA_ENV_NAME%"
 
 :: Set the QT_PLUGIN_PATH environment variable
@@ -30,6 +34,9 @@ set QT_PLUGIN_PATH=%PATH_CONDA%\envs\%CONDA_ENV_NAME%\Library\plugins
 
 :: this will have to be changed, however i don't currently know how to dinamycally change disks on windows
 
-D:
-cd D:\Caio\dlseis_well_tie_petrel\dlseis_well_tie_petrel\bin\Debug
-:: python "%AppData%\dlseis_well_tie\petrel\teste.py" %1 %2 %3 %4
+echo executing code
+python "%AppData%\dlseis_well_tie\petrel\interface2.py" %1 %2 %3 %4 || (
+    echo Erro ao executar o script Python
+    pause
+)
+pause
